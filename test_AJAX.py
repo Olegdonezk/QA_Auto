ee8bdda9b2647c647b31185287f7222908a05b73
Oleg <olegdonezk@gmail.com>
2026-05-13 20:27:43 +0200

    updated test with ITCH input

--- test_AJAX.py
+++ test_AJAX.py
@@ -1,60 +1,70 @@
-# import pytest
-# from selenium import webdriver
-# from selenium.webdriver.common.by import By
-# from selenium.webdriver.support.ui import WebDriverWait
-# from selenium.webdriver.support import expected_conditions as EC
-#
-# @pytest.fixture
-# def driver():
-#     driver = webdriver.Chrome()
-#     driver.get("http://uitestingplayground.com/ajax")
-#     yield driver
-#     driver.quit()
-#
-# def test_ajax_request_explicit(driver):
-#     wait = WebDriverWait(driver, 15)
-#
-#
-#     ajax_button = driver.find_element(By.ID, "ajaxButton")
-#     ajax_button.click()
-#
-#
-#     ajax_text_element = wait.until(
-#         EC.visibility_of_element_located((By.CLASS_NAME, "bg-success"))
-#     )
-#
-#
-#     assert "Data loaded with AJAX get request." in ajax_text_element.text
-
-
 import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service
-
 
 @pytest.fixture
 def driver():
-    service = Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=service)
-    driver.get("https://bonigarcia.dev/selenium-webdriver-java/loading-images.html")
-    yield driver
-    driver.quit()
+     driver = webdriver.Chrome()
+     driver.get("http://uitestingplayground.com/ajax")
+     yield driver
+     driver.quit()
+
+def test_button_text_change(driver):
+
+    driver.get("http://uitestingplayground.com/textinput")
 
 
-def test_loading_images(driver):
-    wait = WebDriverWait(driver, 20)
+    input_field = driver.find_element(By.ID, "newButtonName")
+    input_field.send_keys("ITCH")
 
-    wait.until(
-        lambda d: any(
-            img.get_attribute("alt") == "award"
-            for img in d.find_elements(By.TAG_NAME, "img")
+
+    button = driver.find_element(By.ID, "updatingButton")
+    button.click()
+
+
+    WebDriverWait(driver, 10).until(
+        EC.text_to_be_present_in_element(
+            (By.ID, "updatingButton"),
+            "ITCH"
         )
     )
 
-    images = driver.find_elements(By.TAG_NAME, "img")
 
-    assert any(img.get_attribute("alt") == "award" for img in images)
+    assert button.text == "ITCH"
+
+
+# import pytest
+# from selenium import webdriver
+# from selenium.webdriver.common.by import By
+# from selenium.webdriver.support.ui import WebDriverWait
+# from selenium.webdriver.support import expected_conditions as EC
+# from webdriver_manager.chrome import ChromeDriverManager
+# from selenium.webdriver.chrome.service import Service
+#
+#
+# @pytest.fixture
+# def driver():
+#     service = Service(ChromeDriverManager().install())
+#     driver = webdriver.Chrome(service=service)
+#     driver.get("https://bonigarcia.dev/selenium-webdriver-java/loading-images.html")
+#     yield driver
+#     driver.quit()
+#
+#
+# def test_loading_images(driver):
+#     wait = WebDriverWait(driver, 20)
+#
+#     wait.until(
+#         lambda d: any(
+#             img.get_attribute("alt") == "award"
+#             for img in d.find_elements(By.TAG_NAME, "img")
+#         )
+#     )
+#
+#     images = driver.find_elements(By.TAG_NAME, "img")
+#
+#     assert any(img.get_attribute("alt") == "award" for img in images)
+
+#
